f0e566f42cf7a8f20f8a907a8fddc7a082dd780c
PratikDhumal-js <pratikdhumalapsit@gmail.com>
2020-10-06 23:27:13 +0530

    projects

---
+++ projects/message/script.js
@@ -0,0 +1,12 @@
+const sendBtn = document.querySelector('#sendBtn');
+const messageIn = document.querySelector('#messageIn');
+const messageOut = document.querySelector('messageOut');
+sendBtn.addEventListener('click', sendMsg)
+function sendMsg (){
+    let content = messageIn.value;
+    console.log(content)
+    
+        messageOut.innerHTML = content;
+        messageIn.value = '';
+}
+    
